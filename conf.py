7a469700979f1042040f2523bad26aea34c21749
KunYuChen <18z@users.noreply.github.com>
2019-05-28 11:13:33 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -44,13 +44,16 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme' # alabaster
+# import sphinx_rtd_theme
+# html_theme = 'sphinx_rtd_theme' # alabaster
 # html_theme = 'alabaster' # alabaster
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_logo = "kychen.JPG"
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
